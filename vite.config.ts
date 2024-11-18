ad94f58a425512df1b24381e653a35fbacbe8b63
Sully <147931276+sullytobias@users.noreply.github.com>
2024-11-18 22:13:43 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    base: "./",
+    base: "/world-clock-3d/", // Your repository name
+    build: {
+        outDir: "dist", // Ensure this matches your deployment script
+    },
 });
